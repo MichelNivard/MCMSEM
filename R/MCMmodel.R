6fc88bbb9f1a526aeea54c0e753e608f84e0c3af
mze210 <m.d.vander.zee@vu.nl>
2022-06-14 11:28:04 +0200

    Update 14-06-2022

--- R/MCMmodel.R
+++ R/MCMmodel.R
@@ -20,7 +20,7 @@ MCMmodel <- function(data, n_confounding=1, constrained_a=TRUE, scale_data=TRUE)
   # Scale data
   data_scaled <- apply(data, 2, scale)
   data_was_scaled <- all(round(data, 2) == round(data_scaled, 2))
-  if (scale_data) {
+  if ((scale_data) & !(data_was_scaled)) {
     data <- data_scaled
   }
 
