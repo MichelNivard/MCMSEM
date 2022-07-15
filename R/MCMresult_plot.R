ef0009df2e9711a45069b1e7411d65c1c7bfbd88
Michel Nivard <m.g.nivard@gmail.com>
2022-07-15 13:20:24 +0200

    Update MCMresult_plot.R

--- R/MCMresult_plot.R
+++ R/MCMresult_plot.R
@@ -1,4 +1,4 @@
 plot.mcmresultclass <- function(res,layout=NULL) {
   # As of version 0.6.0 this is ported to plot.mcmmodelclass to prevent duplication of code
-  return(plot(res$model,layout=NULL, use_values = TRUE))
+  return(plot(res$model,layout=layout, use_values = TRUE))
 }
